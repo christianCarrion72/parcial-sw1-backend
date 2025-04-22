dcc69640b4afe708a2c1db3482539202c31c35fd
christianCarrion72 <christianco720@gmail.com>
2025-04-22 00:56:46 -0400

    Primer commit: Inicialización del proyecto

---
+++ src/app.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { User } from './users/entities/user.entity';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT ?? '5432'),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [User],
+      synchronize: true,
+    }),
+    UsersModule,
+    AuthModule
+
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
